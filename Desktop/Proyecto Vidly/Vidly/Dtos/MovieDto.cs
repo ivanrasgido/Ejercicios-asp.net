0a97883dfb67a905601e0ab2db40cb56cafbf065
ivanrasgido <ivanrasgido19@gmail.com>
2021-03-23 13:29:45 -0300

    Ejercicio Unidad 6

---
+++ Desktop/Proyecto Vidly/Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        
+        public byte Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public byte GenreId { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
